bd27769f0e2bae5ed55cac32589a422213bbaece
Uriel Rodriguez <codenameuriel28@gmail.com>
2021-08-19 20:37:03 -0400

    add dependencies and logic to gather html data

---
+++ dist/index.js
@@ -0,0 +1,63 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ChromeLauncher = __importStar(require("chrome-launcher"));
+const puppeteer_core_1 = __importDefault(require("puppeteer-core"));
+const axios_1 = __importDefault(require("axios"));
+const init = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const chrome = yield ChromeLauncher.launch({
+            chromeFlags: ['--headless']
+        });
+        const response = yield axios_1.default.get(`http://localhost:${chrome.port}/json/version`);
+        const { webSocketDebuggerUrl } = response.data;
+        const browser = yield puppeteer_core_1.default.connect({
+            browserWSEndpoint: webSocketDebuggerUrl
+        });
+        const isConnected = browser.isConnected();
+        isConnected && console.log('Connected to browser...');
+        const page = yield browser.newPage();
+        yield page.goto('https://etsy.com/search/vintage?q=gold+jewelry');
+        const aTagHrefs = yield page.$$eval('a.listing-link', (aTags) => aTags.map((aTag) => aTag.href));
+        // console.log(aTagHrefs, aTagHrefs.length);
+        // filter out the '?version=[0-9]' characters from the end of the string
+        const imgTagSources = yield page.$$eval('img[data-listing-card-listing-image]', (imgTags) => imgTags.map((imgTag) => imgTag.src.split(/\?version=[0-9]\b/)[0]));
+        // console.log(imgTagSources, imgTagSources.length);
+        yield browser.close();
+        yield chrome.kill();
+    }
+    catch (err) {
+        console.error(err);
+    }
+});
+init();
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import * as ChromeLauncher from 'chrome-launcher';
+import puppeteer from 'puppeteer-core';
+import axios from 'axios';
+
+const init = async () => {
+  try {
+    const chrome = await ChromeLauncher.launch({
+      chromeFlags: ['--headless']
+    });
+    const response = await axios.get(
+      `http://localhost:${chrome.port}/json/version`
+    );
+    const { webSocketDebuggerUrl } = response.data;
+
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: webSocketDebuggerUrl
+    });
+    const isConnected = browser.isConnected();
+    isConnected && console.log('Connected to browser...');
+
+    const page = await browser.newPage();
+
+    await page.goto('https://etsy.com/search/vintage?q=gold+jewelry');
+
+    const aTagHrefs = await page.$$eval('a.listing-link', (aTags) =>
+      aTags.map((aTag) => (aTag as HTMLAnchorElement).href)
+    );
+    // console.log(aTagHrefs, aTagHrefs.length);
+
+    // filter out the '?version=[0-9]' characters from the end of the string
+    const imgTagSources = await page.$$eval(
+      'img[data-listing-card-listing-image]',
+      (imgTags) => imgTags.map((imgTag) => (imgTag as HTMLImageElement).src.split(/\?version=[0-9]\b/)[0])
+    );
+    // console.log(imgTagSources, imgTagSources.length);
+
+    await browser.close();
+    await chrome.kill();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+init();
